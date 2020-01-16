40bd7dc3a4785153290f812bdc4021a9751d0475
ElizaLU <ELIZA.LUSZCZEWSKA@GMAIL.com>
2020-01-16 17:35:36 +0100

    create basic components

---
+++ src/components/BookList.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Booklist extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Shared Books</h2>
+        <ul id="book-list"></ul>
+      </div>
+    );
+  }
+}
+
+export default Booklist;
