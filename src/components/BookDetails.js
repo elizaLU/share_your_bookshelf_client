701d3470911126993355c4b825366dcc36e0bd14
ElizaLU <ELIZA.LUSZCZEWSKA@GMAIL.com>
2020-01-27 13:25:44 +0100

    display book details if book seleced

--- src/components/BookDetails.js
+++ src/components/BookDetails.js
@@ -4,16 +4,47 @@ import { getBookQuery } from "../queries/queries";
 import { compose } from "redux";
 
 class BookDetails extends Component {
+  displayBookDetails() {
+    const { book } = this.props.getBookQuery;
+    if (book) {
+      return (
+        <div>
+          <h2>{book.title}</h2>
+          <p>
+            {book.author.name} {book.author.surname}
+          </p>
+          <h3>Other books by this author:</h3>
+          <ul className="other-books">
+            {book.author.works.map(work => {
+              return <li key={work.id}>{work.title}</li>;
+            })}
+          </ul>
+        </div>
+      );
+    }
+  }
   render() {
-    // console.log("what are the props", this.props); //need to install CORS on express server
+    console.log("what are the props", this.props.getBookQuery.book); //need to install CORS on express server
+
     return (
       <div id="book-detail">
         <h2>Book Details</h2>
+        {this.displayBookDetails()}
       </div>
     );
   }
 }
 
-export default compose(graphql(getBookQuery, { name: "getBookQuery" }))(
-  BookDetails
-);
+export default compose(
+  graphql(getBookQuery, {
+    name: "getBookQuery",
+    //whenever props.bookId updates - this function will call getBookQuery with selected ID variable
+    options: props => {
+      return {
+        variables: {
+          id: props.bookId
+        }
+      };
+    }
+  })
+)(BookDetails);
--- src/components/BookList.js
+++ src/components/BookList.js
@@ -5,17 +5,25 @@ import { compose } from "redux";
 import BookDetails from "./BookDetails";
 
 class BookList extends Component {
+  state = {
+    selected: null
+  };
   displayBooks() {
     //console.log(this.props);
     //data is attached to props when we bind the query to the component
     const data = this.props.getBooksQuery;
-    //the coolest easiest way to await data loading, and avoid the undefinced hell, I learned so far
+    //the coolest   easiest way to await data loading, and avoid the undefinced hell, I learned so far
     if (data.loading) {
       return <div>Loading books</div>;
     } else {
       return data.books.map(book => {
         return (
-          <div key={book.id}>
+          <div
+            key={book.id}
+            onClick={e => {
+              this.setState({ selected: book.id });
+            }}
+          >
             <li>Title: {book.title}</li>
             <p>{`by ${book.author.name}  ${book.author.surname}`}</p>
             <p>availability: {book.availability.toString()}</p>
@@ -31,7 +39,7 @@ class BookList extends Component {
       <div>
         <h2>Shared Books</h2>
         <ul id="book-list">{this.displayBooks()}</ul>
-        <BookDetails />
+        <BookDetails bookId={this.state.selected} />
       </div>
     );
   }
